eec3f88ee6a142bd6b27d362b91f4a8badc18610
Na-chidor <ermib6@gmail.com>
2025-02-10 00:49:10 -0500

    first commit

---
+++ backend/models/bookModel.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const bookSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publishYear: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User', // Reference to the User model
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Book = mongoose.model('Book', bookSchema);
