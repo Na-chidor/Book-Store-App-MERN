c8294fc298be3cfafd983348318bb2fc8a8c86f0
Na-chidor <ermib6@gmail.com>
2025-02-10 03:44:16 -0500

    cors Middleware update

--- backend/index.js
+++ backend/index.js
@@ -20,9 +20,9 @@ app.use(cors());
 // Option 2: Allow Custom Origins
 // app.use(
 //   cors({
-//     origin: 'http://localhost:3000',
+//     origin: '',
 //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
+//     credentials: true,
 //   })
 // );
 
