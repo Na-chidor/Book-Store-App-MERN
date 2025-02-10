87a256757733cecec880d9b404b4e2957617a11a
Na-chidor <ermib6@gmail.com>
2025-02-10 09:03:43 -0500

    port

--- backend/index.js
+++ backend/index.js
@@ -31,14 +31,13 @@ app.get('/', (request, response) => {
 
 app.use('/books', booksRoute);
 app.use('/auth', authRoute);
-mongoose
-  .connect(mongoDBURL)
-  .then(() => {
-    console.log('App connected to database');
-    app.listen(PORT, () => {
-      console.log(`App is listening to port: ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
+});
+
