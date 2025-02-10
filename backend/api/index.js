c6a29f96dad33e5374e2c224ada5eeadc4cd427f
Na-chidor <ermib6@gmail.com>
2025-02-10 10:04:45 -0500

    cors change

--- backend/api/index.js
+++ backend/api/index.js
@@ -13,15 +13,15 @@ const app = express();
 // Middleware for parsing request body
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
 // Middleware for handling CORS POLICY
 // Option 1: Allow All Origins with Default of cors(*)
 // Option 2: Allow Custom Origins
-// app.use(cors({
-//   origin: "https://book-store-app-mern-lal2.vercel.app", // Allow frontend
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   credentials: true
-// }));
+app.use(cors({
+  origin: "hhttps://book-store-app-mern-phi.vercel.app/", // Allow frontend
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true
+}));
 // );
 
 app.get('/', (request, response) => {
