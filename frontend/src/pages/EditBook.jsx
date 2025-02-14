740a42ecfb8b4db1ab6f2cd1814b38eac9501451
Na-chidor <ermib6@gmail.com>
2025-02-14 07:25:01 -0500

    route to hom

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -47,7 +47,7 @@ const EditBook = () => {
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Edited successfully', { variant: 'success' });
-        navigate('/');
+        navigate('/home');
       })
       .catch((error) => {
         setLoading(false);
