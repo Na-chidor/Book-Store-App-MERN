b3e0b3cb7cc526434faaa27def040902224c405c
Na-chidor <ermib6@gmail.com>
2025-02-16 20:29:40 -0500

    password issue

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -50,7 +50,7 @@ router.post('/register', async (req, res) => {
 
   } catch (error) {
     console.error("Registration Error:", error);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: 'Server error', error: error.message });
   }
 });
 
